717d369fb30a8f7177c08d7c7acd82f16297e230
BhargavMadadi <bhargavmadadi38@gmail.com>
2024-03-03 13:30:52 -0500

    website works yay

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -7,7 +7,14 @@ function App() {
       <div className="App">
         <header className="bg">
           <div className="topnav">
-            {/* Links here */}
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+            <a href="#home" class="active">Home</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+              <i class="fa fa-bars"></i>
+            </a>
+
           </div>
           <div id="box">
             <h1 className="title">EcoEats</h1>
